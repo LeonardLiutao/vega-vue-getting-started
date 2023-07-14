aed98b382011ac05e29e09a0a15ac44405969930
Leonard Liutao <leonard.liutao@activenetwork.com>
2023-07-14 17:12:26 +0800

    Vega vue init

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,14 @@
 <template>
-Hello Vue
+  <h1>Hello Vega Vue</h1>
+
+  <VegaInputSelect
+      :source="[
+        { id: 'male', displayName: 'Male' },
+        { id: 'female', displayName: 'Female' },
+      ]"
+  />
 </template>
 
 <script setup>
+import {VegaInputSelect} from "@heartlandone/vega-vue";
 </script>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,11 @@
 import { createApp } from 'vue'
+import '@heartlandone/vega/style/vega.css';
+import {VegaComponentLibrary} from "@heartlandone/vega-vue";
+
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App).use(VegaComponentLibrary);
+
+VegaComponentLibrary.isReady().then(() => {
+    app.mount('#app');
+});
